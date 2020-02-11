0d14821fa7779e24d0d97df97014a922c129e55e
User1 <User1@LP-5CD9176M5C.HCLT.CORP.HCL.IN>
2020-02-11 13:10:44 +0530

    Adding entities.

---
+++ src/main/java/com/hcl/ing/forextransfer/entity/Transactions.java
@@ -0,0 +1,70 @@
+package com.hcl.ing.forextransfer.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transactions {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long transactionId;
+	private Long accountNumber;
+	private String transactionType;
+	private Double amount;
+	private String currency; 
+	private String description;
+	private String status;
+	private Long userId;
+	
+	public Long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(Long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(Long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+}
